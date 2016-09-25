45a639b7f697748d0bed6de15920cd9bc6eaff04
danielafe7github <daniela.milon@ucsp.edu.pe>
2016-09-25 17:37:31 -0500

    SO

---
+++ SO_02.cpp
@@ -0,0 +1,73 @@
+SO Comunicar procesos con el CPU
+bajrlo en disco cargarlo en memoria
+
+
+
+APP
+------
+SO
+------
+FIRMARE (bios uefi)
+------
+hardware
+
+Antes de entregale el poder al disco duro
+se carga firmware
+
+
+Para pasar e ASEMBLER - 00011; debe conocer la arquitectura el compilador del  SO
+Instrucciones en tiempo de COmpilación:
+CONST     ->
+INLINE    ->
+VOLATILE  ->Evitaba que haya una optimazación del compilador
+	ejemp:
+		A=1         /* volatile A=1*/
+		while(A==1) /*Comparación inesesaria*/
+		{}
+
+		while(1){} 
+STATIC	  ->
+
+	ejemp:
+		
+		static void suma()
+		A.suma() ; sin necesidad de instanciar el constructor
+UNSIGNED  ->
+EXTERN    ->    Se junta las variables que quieras exponer; no copia y pega todo el archivo como el inclide
+
+		.C 			.CPP
+
+	EXTERN	INT			num=8 /*No se pdria sin include o extern(solo el int)*/
+		FLOAT
+		BOOL		
+
+<<<Tare inline y unsigned>>>
+void inline sumar();
+
+const int *a;  -->El valor no cambia
+int const *a;  -->El ptro no se muevo
+int *a const;
+
+/*************************************************/
+
+/usr/src/linux/kernel/sched/sched.h    /*Cabecera del scheduler*/
+
+
+Arduino; placa libre que ofrece interfaces de comunicacion entre ??
+procesarod != microControlador
+		Tinen todo enbevido(todo denro); ahorrar espacio;Ejemp smarth dragon; todos los procesamientos se hacen dentro
+
+Arduino tiene MicroCOntrolador; ATMEL 
+
+UNO	ATMEGA --> 8 KB
+MEGA	ATMEGA --> 16 KB
+DUE	ATMEGA --> 64 KB
+
+En ese espaci debe de entrar un SO; 
+SO pequeñitos : femtoa; arduos ; twios ;freetos.
+lUNES; ENVIAR NOMBRES; INLINEm; LEER SCHEDULER;ARDUINO.
+
+
+
+
+		
